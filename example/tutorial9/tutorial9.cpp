44af1862af75904b033f0b3b7f3cd9ed5ff14bb7
Antony Polukhin <antoshkka@gmail.com>
2015-01-27 16:57:25 +0300

    Added an example of importing a C windows function from dll (refs #8)

---
+++ example/tutorial9/tutorial9.cpp
@@ -0,0 +1,38 @@
+// Copyright 2015 Antony Polukhin.
+//
+// Distributed under the Boost Software License, Version 1.0.
+// (See accompanying file LICENSE_1_0.txt
+// or copy at http://www.boost.org/LICENSE_1_0.txt)
+
+#include <boost/predef/os.h>
+#if BOOST_WINDOWS
+
+//[callplugcpp_tutorial9
+#include <boost/dll/import.hpp> // for import
+#include <iostream>
+
+namespace dll = boost::dll;
+
+int main()
+{
+    typedef unsigned long (timeGetTime_fn)();       // function signature
+    boost::function<timeGetTime_fn> plugin 
+        = dll::import<timeGetTime_fn>(              // we import using C name, not alias! Using `import<>`
+            "winmm.dll",                            // Windows dll
+            "timeGetTime"                           // function name
+        );
+
+    std::cout << "timeGetTime() returned " << plugin() << std::endl;
+    return 0;
+}
+
+//]
+
+#else // BOOST_WINDOWS
+
+int main()
+{
+    return 0;
+}
+
+#endif // BOOST_WINDOWS
