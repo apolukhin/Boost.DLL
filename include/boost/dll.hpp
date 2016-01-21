ef2ef65af197c68feb2cde7bd82d19175d5113f4
klemens-morgenstern <klemens.morgenstern@gmx.net>
2016-01-21 23:40:23 +0100

    excluded smart_lib via _cplusplus define and added c++1y to travis.

--- include/boost/dll.hpp
+++ include/boost/dll.hpp
@@ -18,7 +18,10 @@
 #include <boost/dll/import.hpp>
 #include <boost/dll/library_info.hpp>
 #include <boost/dll/runtime_symbol_info.hpp>
+
+#if __cplusplus > 201402L
 #include <boost/dll/smart_library.hpp>
+#endif
 
 #ifdef BOOST_HAS_PRAGMA_ONCE
 # pragma once
