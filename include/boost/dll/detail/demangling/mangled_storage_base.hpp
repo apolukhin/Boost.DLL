0197eeb228348fa03a4d0591b03efcd3e931a9a4
klemens-morgenstern <klemens.morgenstern@gmx.net>
2016-02-06 23:44:07 +0100

    switch type_index to cttl.
    this allows incomplete types as overloads, which will come in handy for class import.

--- include/boost/dll/detail/demangling/mangled_storage_base.hpp
+++ include/boost/dll/detail/demangling/mangled_storage_base.hpp
@@ -12,7 +12,8 @@
 #include <map>
 #include <boost/dll/detail/demangling/demangle_symbol.hpp>
 #include <boost/dll/library_info.hpp>
-#include <boost/type_index/stl_type_index.hpp>
+#include <boost/type_index/ctti_type_index.hpp>
+#include <boost/type_traits/remove_reference.hpp>
 
 namespace boost { namespace dll { namespace detail {
 
@@ -33,7 +34,7 @@ struct mangled_storage_base
 protected:
     std::vector<entry> storage_;
     ///if a unknown class is imported it can be overloaded by this type
-    std::map<boost::typeindex::stl_type_index, std::string> aliases_;
+    std::map<boost::typeindex::ctti_type_index, std::string> aliases_;
 public:
     void assign(const mangled_storage_base & storage)
     {
@@ -54,7 +55,7 @@ public:
     template<typename T>
     std::string get_name() const
     {
-        auto tx = boost::typeindex::stl_type_index::type_id<T>();
+        auto tx = boost::typeindex::ctti_type_index::type_id<T>();
         auto val = (aliases_.count(tx) > 0) ? aliases_.at(tx) : tx.pretty_name();
         return val;
     }
@@ -93,7 +94,7 @@ public:
     template<typename Alias> void add_alias(const std::string& name)
     {
         aliases_.emplace(
-            boost::typeindex::stl_type_index::type_id<Alias>(),
+            boost::typeindex::ctti_type_index::type_id<Alias>(),
             name
             );
     }
