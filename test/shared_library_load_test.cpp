b0e0fc1da19aeefb6176204194c705612d00409c
Antony Polukhin <antoshkka@gmail.com>
2016-02-08 23:45:24 +0300

    Windows related workaround (2)

--- test/shared_library_load_test.cpp
+++ test/shared_library_load_test.cpp
@@ -218,8 +218,11 @@ int main(int argc, char* argv[])
    {
         try {
 #if BOOST_OS_WINDOWS
+            std::cerr << 1;
             boost::dll::shared_library("winmm.dll");
-            BOOST_TEST(!boost::dll::shared_library(L"Kernel32.dll").has("AddDllDirectory"));
+            std::cerr << 2;
+            BOOST_TEST(!boost::dll::shared_library(L"Kernel32.dll", load_mode::search_system_folders).has("AddDllDirectory"));
+            std::cerr << 3;
 #elif BOOST_OS_LINUX
             boost::dll::shared_library("libdl.so");
             BOOST_TEST(false);
@@ -230,7 +233,9 @@ int main(int argc, char* argv[])
    {
         try {
 #if BOOST_OS_WINDOWS
+            std::cerr << 4;
             boost::dll::shared_library("winmm", load_mode::search_system_folders | load_mode::append_decorations);
+            std::cerr << 5;
 #elif BOOST_OS_LINUX
             boost::dll::shared_library("dl", boost::dll::load_mode::search_system_folders | load_mode::append_decorations);
 #endif
