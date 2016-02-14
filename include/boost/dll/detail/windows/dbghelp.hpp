6b4c0eb209f3ee4d750b19beb177c4c74eda32f8
Andrey Semashev <andrey.semashev@gmail.com>
2016-02-14 14:36:25 +0300

    Resolved namespace and macro conflicts with Boost.WinAPI.

--- include/boost/dll/detail/windows/dbghelp.hpp
+++ include/boost/dll/detail/windows/dbghelp.hpp
@@ -5,12 +5,16 @@
 //  Distributed under the Boost Software License, Version 1.0.
 //  See http://www.boost.org/LICENSE_1_0.txt
 
-#ifndef BOOST_DETAIL_WINAPI_DBGHELP_HPP_
-#define BOOST_DETAIL_WINAPI_DBGHELP_HPP_
-
+#ifndef BOOST_DLL_DETAIL_WINDOWS_DBGHELP_HPP_
+#define BOOST_DLL_DETAIL_WINDOWS_DBGHELP_HPP_
 
 #include <boost/detail/winapi/basic_types.hpp>
+
+struct API_VERSION;
+
 namespace boost
+{
+namespace dll
 {
 namespace detail
 {
@@ -19,32 +23,32 @@ namespace winapi
 extern "C"
 {
 
-BOOST_SYMBOL_IMPORT DWORD_ WINAPI UnDecorateSymbolName
-        (const CHAR_ * DecoratedName,
-         CHAR_ *UnDecoratedName,
-         DWORD_ UndecoratedLength,
-         DWORD_ Flags);
+BOOST_SYMBOL_IMPORT boost::detail::winapi::DWORD_ WINAPI UnDecorateSymbolName
+        (const boost::detail::winapi::CHAR_ * DecoratedName,
+         boost::detail::winapi::CHAR_ *UnDecoratedName,
+         boost::detail::winapi::DWORD_ UndecoratedLength,
+         boost::detail::winapi::DWORD_ Flags);
 
-BOOST_SYMBOL_IMPORT DWORD_ WINAPI UnDecorateSymbolNameW
-        (const WCHAR_ * DecoratedName,
-         WCHAR_* UnDecoratedName,
-         DWORD_ UndecoratedLength,
-         DWORD_ Flags);
+BOOST_SYMBOL_IMPORT boost::detail::winapi::DWORD_ WINAPI UnDecorateSymbolNameW
+        (const boost::detail::winapi::WCHAR_ * DecoratedName,
+         boost::detail::winapi::WCHAR_* UnDecoratedName,
+         boost::detail::winapi::DWORD_ UndecoratedLength,
+         boost::detail::winapi::DWORD_ Flags);
 
 }
 
-inline DWORD_ undecorate_symbol_name(const char* decorated_name,
+inline boost::detail::winapi::DWORD_ undecorate_symbol_name(const char* decorated_name,
                               char*  undecorated_name,
-                              DWORD_ undecorated_length,
-                              DWORD_ flags)
+                              boost::detail::winapi::DWORD_ undecorated_length,
+                              boost::detail::winapi::DWORD_ flags)
 {
     return UnDecorateSymbolName(decorated_name, undecorated_name, undecorated_length, flags);
 }
 
-inline DWORD_ undecorate_symbol_name(const wchar_t* decorated_name,
+inline boost::detail::winapi::DWORD_ undecorate_symbol_name(const wchar_t* decorated_name,
                               wchar_t*  undecorated_name,
-                              DWORD_ undecorated_length,
-                              DWORD_ flags)
+                              boost::detail::winapi::DWORD_ undecorated_length,
+                              boost::detail::winapi::DWORD_ flags)
 {
     return UnDecorateSymbolNameW(decorated_name, undecorated_name, undecorated_length, flags);
 }
@@ -59,9 +63,7 @@ struct api_version {
   unsigned short reserved;
 };
 
-struct _API_VERSION;
-
-_API_VERSION* WINAPI ImagehlpApiVersion();
+BOOST_SYMBOL_IMPORT ::API_VERSION* WINAPI ImagehlpApiVersion();
 
 
 
@@ -71,9 +73,9 @@ inline api_version& image_api_version()
     return *reinterpret_cast<api_version*>(ImagehlpApiVersion());
 }
 
-}}}
+}}}}
 
 
 
 
-#endif /* INCLUDE_BOOST_DETAIL_WINAPI_DBGHELP_HPP_ */
+#endif /* BOOST_DLL_DETAIL_WINDOWS_DBGHELP_HPP_ */
