28f820d35113e240aa341db36eadd70a472ae342
Antony Polukhin <antoshkka@gmail.com>
2016-03-10 23:12:43 +0300

    Docs updated

--- example/getting_started_library.cpp
+++ example/getting_started_library.cpp
@@ -11,12 +11,16 @@
 #include <boost/dll.hpp>
 #include <string>
 
+#ifdef BOOST_NO_CXX11_NOEXCEPT
+#define noexcept
+#endif
+
 #define API extern "C" BOOST_SYMBOL_EXPORT
 
 #ifndef BOOST_NO_CXX11_RVALUE_REFERENCES
 //[getting_started_exports_cpp11_function
 namespace some_namespace {
-    API int i_am_a_cpp11_function(std::string&& param);
+    API int i_am_a_cpp11_function(std::string&& param) noexcept;
 //          ^--------------------  function name to use in dll::import<>
 }
 //]
@@ -33,7 +37,7 @@ namespace your_project_namespace {
 
 //[getting_started_exports_alias
 namespace some_namespace {
-    std::string i_am_function_with_ugly_name(const std::string& param);
+    std::string i_am_function_with_ugly_name(const std::string& param) noexcept;
 }
 
 // When you have no control over function sources or wish to specify another name.
