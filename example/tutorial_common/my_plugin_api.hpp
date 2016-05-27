c82cb99ec931326b498d6c6a5b8414c28c20c54a
klemens-morgenstern <klemens-morgenstern@users.noreply.github.com>
2016-05-27 18:49:40 +0200

    Fixed errornous commit

--- example/tutorial_common/my_plugin_api.hpp
+++ example/tutorial_common/my_plugin_api.hpp
@@ -4,8 +4,8 @@
 // (See accompanying file LICENSE_1_0.txt
 // or copy at http://www.boost.org/LICENSE_1_0.txt)
 
-#ifndef BOOST_DLL_MY_CPP_PLUGIN_API_HPP
-#define BOOST_DLL_MY_CPP_PLUGIN_API_HPP
+#ifndef BOOST_DLL_MY_PLUGIN_API_HPP
+#define BOOST_DLL_MY_PLUGIN_API_HPP
 
 //[plugapi
 #include <string>
