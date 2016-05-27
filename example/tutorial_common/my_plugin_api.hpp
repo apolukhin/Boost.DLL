22273044403e43c4a67251cccfe87560bc59918a
klemens-morgenstern <klemens.morgenstern@gmx.net>
2016-05-27 18:48:15 +0200

    first bit of doc for mangled stuff

--- example/tutorial_common/my_plugin_api.hpp
+++ example/tutorial_common/my_plugin_api.hpp
@@ -1,13 +1,11 @@
-// Copyright 2011-2013 Renato Tegon Forti
-// Copyright 2014 Renato Tegon Forti, Antony Polukhin.
-// Copyright 2015 Antony Polukhin.
+// Copyright 2016 Antony Polukhin.
 //
 // Distributed under the Boost Software License, Version 1.0.
 // (See accompanying file LICENSE_1_0.txt
 // or copy at http://www.boost.org/LICENSE_1_0.txt)
 
-#ifndef BOOST_DLL_MY_PLUGIN_API_HPP
-#define BOOST_DLL_MY_PLUGIN_API_HPP
+#ifndef BOOST_DLL_MY_CPP_PLUGIN_API_HPP
+#define BOOST_DLL_MY_CPP_PLUGIN_API_HPP
 
 //[plugapi
 #include <string>
