8c641504e9a7ee6ccb16dfa77ed1fc089f4a9675
Antony Polukhin <antoshkka@gmail.com>
2018-10-03 20:38:39 +0300

    Fix warning
    `warning: ignoring return value of function declared with const attribute`

--- test/symbol_runtime_info_test.cpp
+++ test/symbol_runtime_info_test.cpp
@@ -24,7 +24,7 @@
 // Makes global error variables dirty. Useful for preventing issues like https://github.com/boostorg/dll/issues/16
 void make_error_code_dirty() {
     using namespace std;
-    log(-1.0);
+    (void)log(-1.0);
 
 #if BOOST_OS_WINDOWS
     boost::winapi::WCHAR_ path_hldr[10];
