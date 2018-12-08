95202e9e17dc50b97ff968d2b2d814586ea9fd97
Antony Polukhin <antoshkka@gmail.com>
2018-12-08 15:10:18 +0300

    Fix UB with visibility of base class

--- example/tutorial_common/my_plugin_api.hpp
+++ example/tutorial_common/my_plugin_api.hpp
@@ -1,4 +1,4 @@
-// Copyright 2016-2017 Antony Polukhin.
+// Copyright 2016-2018 Antony Polukhin.
 //
 // Distributed under the Boost Software License, Version 1.0.
 // (See accompanying file LICENSE_1_0.txt
@@ -10,7 +10,7 @@
 //[plugapi
 #include <string>
 
-class my_plugin_api {
+class BOOST_SYMBOL_VISIBLE my_plugin_api {
 public:
    virtual std::string name() const = 0;
    virtual float calculate(float x, float y) = 0;
