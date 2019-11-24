2e5101d0a77c17fe979c3d1aced9acf4b92e3995
Ramil <ramil2085@mail.ru>
2019-11-24 17:39:38 +0300

    Added a copyright note

--- test/template_method_linux_test.cpp
+++ test/template_method_linux_test.cpp
@@ -1,3 +1,9 @@
+// Copyright 2019 Ramil Gauss. 
+// 
+// Distributed under the Boost Software License, Version 1.0. 
+// (See accompanying file LICENSE_1_0.txt 
+// or copy at http://www.boost.org/LICENSE_1_0.txt) 
+
 // For only Linux testing
 
 #include <boost/dll/runtime_symbol_info.hpp>
