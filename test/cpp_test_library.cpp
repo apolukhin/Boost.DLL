af8912d8c83120bf2d1d40eae1e212495b7b442f
xlc <564112343@qq.com>
2019-12-03 17:56:44 +0800

    Fixed branch conflicts

--- test/cpp_test_library.cpp
+++ test/cpp_test_library.cpp
@@ -8,7 +8,8 @@
 
 #include <boost/predef.h>
 
-#if (__cplusplus >= 201402L) || (BOOST_COMP_MSVC >= BOOST_VERSION_NUMBER(14,0,0))
+#if (__cplusplus >= 201402L) ||                                                \
+  (BOOST_COMP_MSVC >= BOOST_VERSION_NUMBER(14, 0, 0))
 
 #include <boost/dll/config.hpp>
 #include <boost/variant.hpp>
@@ -19,186 +20,162 @@ int unscoped_var = 42;
 BOOST_SYMBOL_EXPORT extern const double unscoped_c_var;
 const double unscoped_c_var = 1.234;
 
-
 namespace some_space {
 
 BOOST_SYMBOL_EXPORT extern double variable;
-double variable =  0.2;
-BOOST_SYMBOL_EXPORT const int & scoped_fun()
+double variable = 0.2;
+BOOST_SYMBOL_EXPORT const int&
+scoped_fun()
 {
-    static int x = 0xDEADBEEF;
-    return x;
+  static int x = 0xDEADBEEF;
+  return x;
 }
 
 }
 
-
-BOOST_SYMBOL_EXPORT void overloaded(const volatile int i)
+BOOST_SYMBOL_EXPORT void
+overloaded(const volatile int i)
 {
-    unscoped_var = i;
+  unscoped_var = i;
 }
 
-BOOST_SYMBOL_EXPORT void overloaded(const double d)
+BOOST_SYMBOL_EXPORT void
+overloaded(const double d)
 {
-    some_space::variable = d;
+  some_space::variable = d;
 }
 
-BOOST_SYMBOL_EXPORT void use_variant(boost::variant<int, double> & v)
+BOOST_SYMBOL_EXPORT void
+use_variant(boost::variant<int, double>& v)
 {
-    v = 42;
+  v = 42;
 }
 
-BOOST_SYMBOL_EXPORT void use_variant(boost::variant<double, int> & v)
+BOOST_SYMBOL_EXPORT void
+use_variant(boost::variant<double, int>& v)
 {
-    v = 3.124;
+  v = 3.124;
 }
 
-
-
-namespace some_space
-{
+namespace some_space {
 
 BOOST_SYMBOL_EXPORT extern int father_value;
 int father_value = 12;
 
 struct BOOST_SYMBOL_EXPORT some_father
 {
-    some_father() {  father_value = 24; };
-    ~some_father() { father_value = 112; };
+  some_father() { father_value = 24; };
+  ~some_father() { father_value = 112; };
 };
-
-
 
 struct BOOST_SYMBOL_EXPORT some_class : some_father
 {
-    static int value ;
-    static void set_value(const int &i);
+  static int value;
+  static void set_value(const int& i);
 
   //  static some_class* dummy();
 
-    virtual double  func(double i, double j);
-    virtual int     func(int i,     int j);
-    int func(int i,     int j)             volatile;
-    double func(double i, double j) const volatile;
+  virtual double func(double i, double j);
+  virtual int func(int i, int j);
+  int func(int i, int j) volatile;
+  double func(double i, double j) const volatile;
 
-    int mem_val;
-    int  get() const ;
-    void set(int i)  ;
+  int mem_val;
+  int get() const;
+  void set(int i);
 
-    some_class();
-    some_class(some_class &&);
-    some_class(int i);
+  some_class();
+  some_class(some_class&&);
+  some_class(int i);
 
-    some_class& operator=(some_class &&);
+  some_class& operator=(some_class&&);
 
-
-    virtual ~some_class();
+  virtual ~some_class();
 };
 
-some_class::some_class(some_class &&){}
+some_class::some_class(some_class&&) {}
 
-
-some_class& some_class::operator=(some_class &&ref) {return ref;}
-
+some_class&
+some_class::operator=(some_class&& ref)
+{
+  return ref;
+}
 
 BOOST_SYMBOL_EXPORT extern std::size_t size_of_some_class;
 std::size_t size_of_some_class = sizeof(some_space::some_class);
 
-
-extern "C"  BOOST_SYMBOL_EXPORT const volatile some_class* this_;
-const volatile some_class * this_ = nullptr;
+extern "C" BOOST_SYMBOL_EXPORT const volatile some_class* this_;
+const volatile some_class* this_ = nullptr;
 
 int some_class::value = -1;
 
-void some_class::set_value(const int &i) {value = i;}
+void
+some_class::set_value(const int& i)
+{
+  value = i;
+}
 
+// some_class* some_class::dummy() {return new some_class();}//so it implements
+// an allocating ctor.
 
-//some_class* some_class::dummy() {return new some_class();}//so it implements an allocating ctor.
+double
+some_class::func(double i, double j)
+{
+  this_ = this;
+  return i * j;
+}
+int
+some_class::func(int i, int j)
+{
+  this_ = this;
+  return i + j;
+}
+int
+some_class::func(int i, int j) volatile
+{
+  this_ = this;
+  return i - j;
+  ;
+}
+double
+some_class::func(double i, double j) const volatile
+{
+  this_ = this;
+  return i / j;
+}
 
-double  some_class::func(double i, double j)                {this_ = this; return i*j;}
-int     some_class::func(int i,     int j)                  {this_ = this; return i+j;}
-int     some_class::func(int i,     int j)   volatile       {this_ = this; return i-j;;}
-double  some_class::func(double i, double j) const volatile {this_ = this; return i/j;}
+int
+some_class::get() const
+{
+  this_ = this;
+  return mem_val;
+}
+void
+some_class::set(int i)
+{
+  this_ = this;
+  mem_val = i;
+}
 
-int  some_class::get() const {this_ = this; return mem_val;}
-void some_class::set(int i)  {this_ = this; mem_val = i;}
-
-some_class::some_class()        { this_ = this; value = 23; mem_val = 123;}
-some_class::some_class(int i) : mem_val(456) {this_ = this; value = i;}
+some_class::some_class()
+{
+  this_ = this;
+  value = 23;
+  mem_val = 123;
+}
+some_class::some_class(int i)
+  : mem_val(456)
+{
+  this_ = this;
+  value = i;
+}
 
 some_class::~some_class()
 {
-    value = 0;
-    this_ = this;
+  value = 0;
+  this_ = this;
 }
 
 }
-
-namespace space {
-
-template <typename... T>
-class test_template_class {};
-
-template <typename T1,
-         typename T2 = char,
-         typename T3 = test_template_class<int>>
-class test_template_class_have_default_args {};
-
-class BOOST_SYMBOL_EXPORT cpp_plugin_type_pasrser {
-private:
-    /* data */
-public:
-    cpp_plugin_type_pasrser(/* args */);
-    cpp_plugin_type_pasrser(int*);
-    cpp_plugin_type_pasrser(const int*);
-    cpp_plugin_type_pasrser(const volatile int*);
-    cpp_plugin_type_pasrser(const std::string&);
-    cpp_plugin_type_pasrser(const volatile std::string*);
-    ~cpp_plugin_type_pasrser();
-    void type_test(int*);
-    void type_test(const int*);
-    void type_test(const volatile int*);
-    void type_test(std::string*);
-    void type_test(const std::string*);
-    void type_test(const volatile std::string*);
-    void type_test(const std::string&);
-    void type_test(const test_template_class<>&);
-    void type_test(const test_template_class<int>&);
-    void type_test(const test_template_class<std::string>&);
-    void type_test(const test_template_class<char, int, std::string>&);
-    void type_test(const test_template_class_have_default_args<int>&);
-    void type_test(const test_template_class_have_default_args<int, double>&);
-    void type_test(
-        const test_template_class_have_default_args<int, double, std::string>&);
-};
-
-cpp_plugin_type_pasrser::cpp_plugin_type_pasrser(/* args */) {}
-cpp_plugin_type_pasrser::cpp_plugin_type_pasrser(int*) {}
-cpp_plugin_type_pasrser::cpp_plugin_type_pasrser(const int*) {}
-cpp_plugin_type_pasrser::cpp_plugin_type_pasrser(const volatile int*) {}
-cpp_plugin_type_pasrser::cpp_plugin_type_pasrser(const std::string&) {}
-cpp_plugin_type_pasrser::cpp_plugin_type_pasrser(const volatile std::string*) {}
-cpp_plugin_type_pasrser::~cpp_plugin_type_pasrser() {}
-void cpp_plugin_type_pasrser::type_test(int*) {}
-void cpp_plugin_type_pasrser::type_test(const int*) {}
-void cpp_plugin_type_pasrser::type_test(const volatile int*) {}
-void cpp_plugin_type_pasrser::type_test(std::string*) {}
-void cpp_plugin_type_pasrser::type_test(const std::string*) {}
-void cpp_plugin_type_pasrser::type_test(const volatile std::string*) {}
-void cpp_plugin_type_pasrser::type_test(const std::string&) {}
-void cpp_plugin_type_pasrser::type_test(const test_template_class<>&) {}
-void cpp_plugin_type_pasrser::type_test(const test_template_class<int>&) {}
-void cpp_plugin_type_pasrser::type_test(
-    const test_template_class<std::string>&) {}
-void cpp_plugin_type_pasrser::type_test(
-    const test_template_class<char, int, std::string>&) {}
-void cpp_plugin_type_pasrser::type_test(
-    const test_template_class_have_default_args<int>&) {}
-void cpp_plugin_type_pasrser::type_test(
-    const test_template_class_have_default_args<int, double>&) {}
-void cpp_plugin_type_pasrser::type_test(
-    const test_template_class_have_default_args<int, double, std::string>&) {}
-
-}  // namespace space
 
 #endif
