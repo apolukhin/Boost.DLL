ab379539806956b4113050ebb978615a4f6a09f0
Antony Polukhin <antoshkka@gmail.com>
2020-02-01 19:31:08 +0300

    fix testing with C++98

--- include/boost/dll/smart_library.hpp
+++ include/boost/dll/smart_library.hpp
@@ -1,4 +1,5 @@
 //  Copyright 2016 Klemens Morgenstern
+//  Copyright 2019-2020 Antony Polukhin
 //
 // Distributed under the Boost Software License, Version 1.0.
 // (See accompanying file LICENSE_1_0.txt
@@ -8,7 +9,7 @@
 #define BOOST_DLL_SMART_LIBRARY_HPP_
 
 /// \file boost/dll/smart_library.hpp
-/// \warning Extremely experimental! Requires C++14! Will change in next version of Boost! boost/dll/smart_library.hpp is not included in boost/dll.hpp
+/// \warning Extremely experimental! Requires C++11! Will change in next version of Boost! boost/dll/smart_library.hpp is not included in boost/dll.hpp
 /// \brief Contains the boost::dll::experimental::smart_library class for loading mangled symbols.
 
 #include <boost/dll/config.hpp>
@@ -16,6 +17,10 @@
 #   include <boost/dll/detail/demangling/msvc.hpp>
 #else
 #   include <boost/dll/detail/demangling/itanium.hpp>
+#endif
+
+#if (__cplusplus < 201103L)
+#  error This file requires C++11 at least!
 #endif
 
 #include <boost/dll/shared_library.hpp>
--- test/cpp_load_test.cpp
+++ test/cpp_load_test.cpp
@@ -1,4 +1,5 @@
 // Copyright 2016 Klemens Morgenstern
+// Copyright 2019-2020 Antony Polukhin
 //
 // Distributed under the Boost Software License, Version 1.0.
 // (See accompanying file LICENSE_1_0.txt
@@ -8,7 +9,10 @@
 
 #include <boost/predef.h>
 
-#include <boost/dll/smart_library.hpp>
+#if (__cplusplus >= 201103L)
+// Make sure that it at least compiles
+#  include <boost/dll/smart_library.hpp>
+#endif
 
 #if (__cplusplus >= 201402L) || (BOOST_COMP_MSVC >= BOOST_VERSION_NUMBER(14,0,0))
 
