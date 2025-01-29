ae605ac31def2513ae36c47a86b562ebaac68c7a
Antony Polukhin <antoshkka@gmail.com>
2025-01-29 10:17:09 +0300

    Fixes

--- test/shared_library_get_symbol_test.cpp
+++ test/shared_library_get_symbol_test.cpp
@@ -192,8 +192,12 @@ int main(int argc, char* argv[]) {
     int& reference_to_internal_integer = sl.get<int&>("reference_to_internal_integer");
     BOOST_TEST(reference_to_internal_integer == 0xFF0000);
 
+#if defined(__has_feature)
+# if !__has_feature(address_sanitizer)
     int&& rvalue_reference_to_internal_integer = sl.get<int&&>("rvalue_reference_to_internal_integer");
     BOOST_TEST(rvalue_reference_to_internal_integer == 0xFF0000);
+# endif
+#endif
 
     return boost::report_errors();
 }
